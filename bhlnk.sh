3ae321822da18e13cc9fa594a100e29e10ea79dc
buihien224 <buihien224@gmail.com>
2021-11-16 17:58:31 +0700

    pre_alpha version , sr I forgot commit

--- bhlnk.sh
+++ bhlnk.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 part=(system product system_ext vendor)
+dir=$(pwd)
+bin="$dir/bin/linux"
+bro="$dir/zip_temp"
 super()
 {
 echo "#############################"
@@ -17,7 +21,8 @@ zipfile()
 
 	mkdir zip_temp
 	mkdir temp
-	unzip stock/ziprom.zip -d zip_temp
+	unzip -t ziprom.zip
+	unzip ziprom.zip -d zip_temp
 	for ((i = 0 ; i < 4 ; i++)); do
 		./bin/brotli --decompress zip_temp/"${part[$i]}.new.dat.br" -o zip_temp/"${part[$i]}.new.dat"
 		./bin/sdat2img.py zip_temp/"${part[$i]}.transfer.list" zip_temp/"${part[$i]}.new.dat" "${part[$i]}.img"
@@ -56,7 +61,10 @@ done
 mount()
 {
 clear
-echo "Start mountting ...."
+echo "#############################"
+echo "#        Mounting ....     #"
+echo "#############################"
+echo ""
 echo "Enter your password to use Sudo ...."
 for ((i = 0 ; i < 4 ; i++)); do
 	mkdir temp/"${part[$i]}" 
@@ -72,7 +80,7 @@ echo "#############################"
 echo ""
 echo "Debloating ...."
 list=`cat debloat.txt`
-cd temp/system
+cd $dir/temp/system
   for app in $list; do
         sudo rm -r "$app"
         echo "done"
@@ -86,7 +94,7 @@ echo "#############################"
 echo "#        Unmounting ....    #"
 echo "#############################"
 echo ""
-cd ..
+cd $dir/temp
 sleep 3
 for ((i = 0 ; i < 4 ; i++)); do
 	sudo umount "${part[$i]}"
@@ -101,53 +109,137 @@ echo "#############################"
 echo "#        Shrinking ....     #"
 echo "#############################"
 echo ""
-cd ..
+cd $dir
 sleep 1
 for ((i = 0 ; i < 4 ; i++)); do
 	resize2fs -f -M "${part[$i]}.img"
 	echo "Shrink "${part[$i]}" :  done"
 done
-echo "#############################"
-echo "#          DONE ....        #"
-echo "#############################"
-echo ""
 }
 cleanup()
 {
+	cd $dir
 	if [[ -d "temp" ]]; then
 		rm -r temp
 		rm -r zip_temp
-		rm system.img
-		rm product.img
-		rm system_ext.img 
-		rm vendor.img
-		rm odm.img
 	fi
 }
+remove_source()
+{
+echo "#############################"
+echo "#  remove source file ....   #"
+echo "#############################"
+echo ""
+cd $dir/zip_temp
+	for ((i = 0 ; i < 4 ; i++)); do
+		if [[ -f "${part[$i]}.new.dat.br" ]]; then
+			rm "${part[$i]}.new.dat.br"
+			rm "${part[$i]}.new.dat"
+			rm "${part[$i]}.patch.dat" 
+			rm "${part[$i]}.transfer.list"
+		fi
+	done
+cd ..
+}
+vietsub()
+{
+echo "#############################"
+echo "#       VIETSUB-ING ....    #"
+echo "#############################"
+echo ""
+	cd $dir
+	echo "copy bhlnk's overlay and stuff"
+	sudo cp -a vietsub/. $dir/temp/
+	echo "give permisstion ...."
+	per=$(cat permiss.txt)
+	for p in $per; do
+		if [[ -d "$dir/temp/$p" ]]; then
+			sudo chmod 755 $dir/temp/$p
+		else sudo chmod 644 $dir/temp/$p
+		fi
+	done
+	echo "done"
+}
+repackz()
+{
+clear
+echo "#############################"
+echo "#         Compress          #"
+echo "#############################"
+echo ""
+echo "Compress to sparse img .... "
+for ((i = 0 ; i < 4 ; i++)); do
+	./bin/img2simg "${part[$i]}.img" "s_${part[$i]}.img"
+done
+echo "Compress to new.dat .... "
+ 	echo "- Repack syste.img "
+ 	python3 ./bin/linux/img2sdat.py "s_system.img" -o $bro -v 4 
+ 	echo ""
+ 	echo "DONE"
+ 	echo "- Repack product.img "
+ 	python3 ./bin/linux/img2sdat.py "s_product.img" -o $bro -v 4 
+ 	echo ""
+ 	echo "DONE"
+  	echo "- Repack system_ext.img "
+ 	python3 ./bin/linux/img2sdat.py "s_system_ext.img" -o $bro -v 4 
+ 	echo ""
+ 	echo "DONE"
+  	echo "- Repack vendor.img "
+ 	python3 ./bin/linux/img2sdat.py "s_vendor.img" -o $bro -v 4 
+ 	echo ""
+ 	echo "DONE"
 
+#level brotli
+echo "Compress to brotli .... "
+#
+for ((i = 0 ; i < 4 ; i++)); do
+   	echo "- Repack ${part[$i]}.new.dat"
+	$bin/brotli -1 -j -w 24 "$bro/${part[$i]}.new.dat" -o "$bro/${part[$i]}.new.dat.br"
+	rm -rf "${part[$i]}.img"
+	rm -rf "s_${part[$i]}.img"
+	rm -rf "$bro/${part[$i]}.new.dat"
+done
+
+if [ -d $bro/META-INF ]; then
+	echo "- Zipping"
+	[ -f ./new_rom.zip ] && rm -rf ./new_rom.zip
+	$bin/7za a -tzip "$dir/new_rom.zip" $bro/*  
+fi
+
+
+if [ -f "$dir/new_rom.zip" ]; then
+      echo "- Repack done"
+else
+      echo "- Repack error"
+fi
+}
 cleanup
 echo "#############################"
 echo "#         STARTING ....     #"
 echo "#############################"
 echo ""
-#cd stock
-#if [[ -f "ziprom.zip" ]]; then
-	#cd ..
-	#echo "Zip rom detect"
-	#zipfile
-#elif [[ -f "super.img" ]]; then
-	#cd ..
-	#echo "Super.img detect"
-#else wget -O ziprom.zip $1 && cd .. && zipfile
-#fi
-#mkrw
-#mount
-#debloat
-#umount
-#shrink
-
-if [ -f "s_system.img" ]; then
-		echo "- Repack vendor.img "
-		./bin/img2sdat.py s_sytem.img -o $(pwd) -v 4 -p system
-else echo "bug"
+if [[ -f "ziprom.zip" ]]; then
+	echo "Zip rom detect"
+	zipfile
+elif [[ -f "super.img" ]]; then
+	echo "Super.img detect"
+else wget -O ziprom.zip $1 && cd .. && zipfile
 fi
+mkrw
+mount
+###############
+printf "Do you want remove most unuse app ...\n"
+printf "press y to debloat or n to skip\n"
+read x
+if [[ $x == "Y" ]]; then
+	debloat
+fi
+vietsub
+umount
+shrink
+remove_source
+echo "#############################"
+echo "#         Compress          #"
+echo "#############################"
+echo ""
+repackz
